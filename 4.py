36dd0a4bb0be0eda85bb70bc544396724191a2fb
ShihangWu <2478677199@qq.com>
2025-07-05 15:42:43 +0800

    build a chat bot

--- 4.py
+++ 4.py
@@ -12,13 +12,10 @@ from langchain.chat_models import init_chat_model
 
 model = init_chat_model("gemini-2.0-flash", model_provider="google_genai")
 
-from langchain_core.messages import HumanMessage
+from langchain_core.messages import HumanMessage, AIMessage
 
 from langgraph.checkpoint.memory import MemorySaver
 from langgraph.graph import START, MessagesState, StateGraph
-
-# Define a new graph
-workflow = StateGraph(state_schema=MessagesState)
 
 from typing import Sequence
 
@@ -26,18 +23,49 @@ from langchain_core.messages import BaseMessage
 from langgraph.graph.message import add_messages
 from typing_extensions import Annotated, TypedDict
 
+from langchain_core.messages import SystemMessage, trim_messages
+
+trimmer = trim_messages(
+    max_tokens=65,
+    strategy="last",
+    token_counter=model,
+    include_system=True,
+    allow_partial=False,
+    start_on="human",
+)
+
+messages = [
+    SystemMessage(content="you're a good assistant"),
+    HumanMessage(content="hi! I'm bob"),
+    AIMessage(content="hi!"),
+    HumanMessage(content="I like vanilla ice cream"),
+    AIMessage(content="nice"),
+    HumanMessage(content="whats 2 + 2"),
+    AIMessage(content="4"),
+    HumanMessage(content="thanks"),
+    AIMessage(content="no problem!"),
+    HumanMessage(content="having fun?"),
+    AIMessage(content="yes!"),
+]
+
+trimmer.invoke(messages)
+
 
 class State(TypedDict):
     messages: Annotated[Sequence[BaseMessage], add_messages]
     language: str
 
-workflow = StateGraph(state_schema=MessagesState)
+
+workflow = StateGraph(state_schema=State)
 
 
-def call_model(state: MessagesState):
-    prompt = prompt_template.invoke(state)
+def call_model(state: State):
+    trimmed_messages = trimmer.invoke(state["messages"])
+    prompt = prompt_template.invoke(
+        {"messages": trimmed_messages, "language": state["language"]}
+    )
     response = model.invoke(prompt)
-    return {"messages": response}
+    return {"messages": [response]}
 
 
 workflow.add_edge(START, "model")
@@ -52,16 +80,22 @@ prompt_template = ChatPromptTemplate.from_messages(
     [
         (
             "system",
-            "You talk like a pirate. Answer all questions to the best of your ability.",
+            "You are a helpful assistant. Answer all questions to the best of your ability in {language}.",
         ),
         MessagesPlaceholder(variable_name="messages"),
     ]
 )
 
 # conversation
-config = {"configurable": {"thread_id": "abc345"}}
-query = "Hi! I'm Jim."
+config = {"configurable": {"thread_id": "abc789"}}
+query = "Hi I'm Todd, please tell me a joke."
+language = "English"
 
 input_messages = [HumanMessage(query)]
-output = app.invoke({"messages": input_messages}, config)
-output["messages"][-1].pretty_print()
+for chunk, metadata in app.stream(
+    {"messages": input_messages, "language": language},
+    config,
+    stream_mode="messages",
+):
+    if isinstance(chunk, AIMessage):  # Filter to just model responses
+        print(chunk.content, end="|")
